25a05ec8324ac11fd7ef060b56417226f36e2772
Om <68224663+OmRajadhyaksha@users.noreply.github.com>
2021-05-24 13:30:38 +0530

    Delete Day 3 directory

--- Day 3/assign2.js
+++
@@ -1,6 +0,0 @@
-let input = prompt("Enter your OS name and version:");
-
-let new_input = input.slice( 0, 7 );
-let new_var = input.slice(7);
-
-console.log("The Os name is " + mew_input + " and the version is " + new_var );
